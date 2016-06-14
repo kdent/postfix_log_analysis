d18961b74391b4e31f4bf97e4078bb24df9c579c
kdent <kdent@seaglass.com>
2016-06-14 09:06:49 -0700

    Fixed Bluehost reject.

--- extract_for_labeling.py
+++ extract_for_labeling.py
@@ -16,21 +16,19 @@ with open(sys.argv[1], 'rU') as fh:
     for row in csvrdr:
         if row[7] not in ['jdent@seaglass.com', 'kdent@seaglass.com', 'becky@seaglass.com']:
             continue;
-        if row[12] == 'reject':
+        if row[12] == 'sent':
+            status = 'Accepted'
+        else:
             if 'blocked using zen.spamhaus.org' in row[13]:
                 status = 'Spamhaus Reject'
+            elif 'host mail.seaglass.com' in row[13]:
+                status = 'Bluehost Reject'
             elif 'Helo command rejected' in row[13]:
                 status = 'Helo Reject'
-            elif '550-Verification failed for' in row[13]:
-                status = 'Bluehost Reject'
             elif 'Sender address rejected' in row[13]:
                 status = 'Rule Reject'
             else:
                 status = row[13]
-        elif row[12] == 'sent':
-            status = 'Accepted'
-        else:
-            status = row[13]
-            
+
         newrow = [row[0], row[1], row[6], row[7], '', status]
         csvout.writerow(newrow)
